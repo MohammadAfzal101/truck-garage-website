414ae990f57de2498fde2f35eca8622d1968cc1f
mohammadafzal101 <mohdafzalshaikh101@gmail.com>
2026-01-07 20:46:52 +0530

    Initial commit: Truck Garage website structure

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from .views import home
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('services/', views.services, name='services'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('gallery/', views.gallery, name='gallery'),
+]
